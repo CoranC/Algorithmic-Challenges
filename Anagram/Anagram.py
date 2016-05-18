36d7e2577bdb7603f2e772985fa07043e93730b7
Coran Corbett <coranc@gmail.com>
2016-05-18 21:02:44 +0100

    Adding docstring

--- Anagram/Anagram.py
+++ Anagram/Anagram.py
@@ -1,4 +1,14 @@
 def anagram(s1,s2):
+  """
+  An algorithm that runs in O(n) time by comparing sorted strings.
+
+  Args:
+    s1: (str) Input string assumed to contain no punctuation.
+    s2: (str) Input string assumed to contain no punctuation.
+
+  Returns:
+    A bool indicating whether s1 and s2 are anagrams of each other.
+  """
   s1_sort = sorted(s1.lower().replace(" ", ""))
   s2_sort = sorted(s2.lower().replace(" ", ""))
   if len(s1_sort) != len(s2_sort):
